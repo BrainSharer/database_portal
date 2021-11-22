dda061c5f0a8c2eda11098b21a328feb540d9ed6
Eddy O'Donnell <eddyod@yahoo.com>
2021-11-22 10:32:15 -0500

    Put in explicit make migrations

--- reset.sh
+++ reset.sh
@@ -4,6 +4,9 @@ find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 mysql -e "drop database brainsharer;create database brainsharer;"
 python manage.py makemigrations
+python manage.py makemigrations authentication
+python manage.py makemigrations brain
+python manage.py makemigrations neuroglancer
 python manage.py showmigrations
 python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('edward', 'admin@myproject.com', 'CHANGEME')" | python manage.py shell

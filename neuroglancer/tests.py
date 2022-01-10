2ca7dfede8ea46156eb2dad853d7e4077fe72795
Eddy O'Donnell <eddyod@yahoo.com>
2022-01-11 06:01:38 +0700

    Added more tests and removed the tests that were not needed. This should help with issue #7. We can now run: python manage.py test neuroglancer That runs quite quickly and goes thru the tests successfully

--- neuroglancer/tests.py
+++ neuroglancer/tests.py
@@ -1,73 +1,71 @@
-import json
-import numpy as np
 from rest_framework import status
 from django.test import Client, TransactionTestCase
 from authentication.models import User
 # Create your tests here.
 from neuroglancer.models import NeuroglancerModel
-
+from datetime import datetime
+import json
 
 class TestNeuroglancerModel(TransactionTestCase):
     client = Client()
+    
 
     def setUp(self):
-        super_user = User.objects.create_superuser(username='super',
-                                                   email='super@email.org',
-                                                   password='pass')
-        # ids 168, 188,210,211,209,200
-        pk = 273
-        self.urlModel = NeuroglancerModel.objects.get(pk=pk)
-
-        self.serializer_data = {
-            'url': self.urlModel.url,
-            'user_date': self.urlModel.user_date,
-            'comments': self.urlModel.comments,
-            'person_id': super_user.id
-        }
-
-        self.bad_serializer_data = {
-            'url': None,
-            'user_date': None,
-            'comments': None,
-            'vetted': None,
-            'public': None,
-            'person_id': "18888888888"
-        }
-
+        '''
+        This method gets run every test below.
+        '''
+        self.layer_name = "TEST999"
+        self.state_id = 0
+    
     def test_neuroglancer_url(self):
         response = self.client.get("/neuroglancer")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_rotations_url(self):
-        response = self.client.get("/rotations")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_annotations_url(self):
         response = self.client.get("/annotations")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_rotation_url_with_bad_animal(self):
-        response = self.client.get("/rotation/DK52XXX/manual/2")
-        data = str(response.content, encoding='utf8')
-        data = json.loads(data)
-        translation = data['translation']
-        s = np.sum(translation)
-        self.assertEqual(s, 0, msg="Translation is equal to zero")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_rotation_url_with_good_animal(self):
-        response = self.client.get("/rotation/DK52/manual/2")
-        data = str(response.content, encoding='utf8')
-        data = json.loads(data)
-        translation = data['translation']
-        s = np.sum(translation)
-        self.assertNotEqual(s, 0, msg="Translation is not equal to zero")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
+        
     def test_annotation_url(self):
         response = self.client.get("/annotation/DK39/premotor/1")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_annotation_Atlas_url(self):
-        response = self.client.get("/annotation/Atlas/COM/1")
+        
+        
+    def test_create_state(self):
+        """
+        Ensure we can create a new neuroglancer_state object.
+        """
+        
+        self.super_user = User.objects.create_superuser(username='super',
+                                                   email='super@email.org',
+                                                   password='pass')
+        state = {}
+        state['dimensions'] = {'x':[0.325, 'um'],
+                               'y':[0.325, 'um'],
+                               'z':[20, 'um'] }
+        state['position'] = [100, 100, 100]
+        state['selectedLayer'] = {'visible': True, 'layer': 1}
+        state['crossSectionScale'] = 90
+        state['projectionScale'] = 100
+        state['layers'] = 1
+        state['gpuMemoryLimit'] = 4000000000
+        state['systemMemoryLimit'] = 8000000000
+        state['layout'] = 'xy'
+        
+        data = {}
+        data['neuroglancer_state'] = json.dumps(state)
+        data['user_date'] = '999999'
+        data['comments'] = self.layer_name
+        data['owner_id'] = self.super_user.id
+        data['created'] = datetime.now()
+        data['updated'] =  datetime.now()
+        
+        response = self.client.post('/neuroglancer', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(NeuroglancerModel.objects.count(), 1)
+        self.assertEqual(NeuroglancerModel.objects.get().comments, self.layer_name)
+        self.state_id = NeuroglancerModel.objects.get().id
+        
+        response = self.client.get("/neuroglancer/" + str(self.state_id))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        

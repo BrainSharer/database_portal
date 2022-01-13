ea97fad8450a6af920239a4b4673fe40a5a13a1d
Eddy O'Donnell <eddyod@yahoo.com>
2022-01-13 08:31:50 +0700

    Added more tests to neuroglancer/tests.py
    Updated the neuroglancer to change person_id to owner_id in
    neuroglancer/src/services/state_loader.ts
    This should take care of issue #10

--- neuroglancer/tests.py
+++ neuroglancer/tests.py
@@ -1,10 +1,12 @@
+import os
+from datetime import datetime
+import json
+
 from rest_framework import status
 from django.test import Client, TransactionTestCase
 from authentication.models import User
 # Create your tests here.
 from neuroglancer.models import NeuroglancerModel
-from datetime import datetime
-import json
 
 class TestNeuroglancerModel(TransactionTestCase):
     client = Client()
@@ -31,7 +33,7 @@ class TestNeuroglancerModel(TransactionTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
         
-    def test_create_state(self):
+    def test_create_post_get_state(self):
         """
         Ensure we can create a new neuroglancer_state object.
         """
@@ -39,18 +41,11 @@ class TestNeuroglancerModel(TransactionTestCase):
         self.super_user = User.objects.create_superuser(username='super',
                                                    email='super@email.org',
                                                    password='pass')
-        state = {}
-        state['dimensions'] = {'x':[0.325, 'um'],
-                               'y':[0.325, 'um'],
-                               'z':[20, 'um'] }
-        state['position'] = [100, 100, 100]
-        state['selectedLayer'] = {'visible': True, 'layer': 1}
-        state['crossSectionScale'] = 90
-        state['projectionScale'] = 100
-        state['layers'] = 1
-        state['gpuMemoryLimit'] = 4000000000
-        state['systemMemoryLimit'] = 8000000000
-        state['layout'] = 'xy'
+        
+        parent_path = os.getcwd()
+        jfile = f'{parent_path}/scripts/363.json'
+        state = json.load(open(jfile))
+        
         
         data = {}
         data['neuroglancer_state'] = json.dumps(state)
@@ -69,3 +64,45 @@ class TestNeuroglancerModel(TransactionTestCase):
         response = self.client.get("/neuroglancer/" + str(self.state_id))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         
+    def test_create_post_get__update_get_state(self):
+        """
+        Ensure we can create, post, get, update and get again a 
+        neuroglancer_state object.
+        """
+        
+        self.super_user = User.objects.create_superuser(username='super',
+                                                   email='super@email.org',
+                                                   password='pass')
+        
+        parent_path = os.getcwd()
+        jfile = f'{parent_path}/scripts/363.json'
+        state = json.load(open(jfile))
+        
+        
+        data = {}
+        data['neuroglancer_state'] = json.dumps(state)
+        data['user_date'] = '999999'
+        data['comments'] = self.layer_name
+        data['owner_id'] = self.super_user.id
+        data['created'] = datetime.now()
+        data['updated'] =  datetime.now()
+        
+        #post
+        response = self.client.post('/neuroglancer', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(NeuroglancerModel.objects.count(), 1)
+        self.assertEqual(NeuroglancerModel.objects.get().comments, self.layer_name)
+        self.state_id = NeuroglancerModel.objects.get().id
+        
+        #get
+        response = self.client.get("/neuroglancer/" + str(self.state_id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        new_comment_name = 'New comment name'
+        data['comments'] = new_comment_name
+        #update
+        response = self.client.put('/neuroglancer/' + str(self.state_id), data, 
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(NeuroglancerModel.objects.count(), 1)
+        self.assertEqual(NeuroglancerModel.objects.get().comments, new_comment_name)
+        self.state_id = NeuroglancerModel.objects.get().id

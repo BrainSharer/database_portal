f291adee2b2b087f9a9486adaf7df468b881fef0
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-05-03 16:47:13 +0700

    Updated the serializer for registration

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,30 +1,11 @@
-from wsgiref import validate
 from authentication.models import User
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True, 'required': True}}
-
-    def create(self, validated_data):
-        '''
-        Check for existing user,
-        add to anonymous lab
-        '''
-        user = User.objects.create_user(**validated_data)
-        return user 
-
+from django.core.exceptions import ValidationError
 
 class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-            required=True,
-            validators=[UniqueValidator(queryset=User.objects.all())]
-            )
-
+    email = serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
@@ -36,21 +17,19 @@ class RegisterSerializer(serializers.ModelSerializer):
             'last_name': {'required': True}
         }
 
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-
-        return attrs
-
     def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
-        )
+        print('cccccccccccccccccccccccccc')
+        try:
+            user = User.objects.create(
+                username=validated_data['username'],
+                email=validated_data['email'],
+                first_name=validated_data['first_name'],
+                last_name=validated_data['last_name']
+            )
+        except ValidationError as ex:
+            print('do we get ehre')
+            raise serializers.ValidationError(f'error {ex}')    
 
-        
         user.set_password(validated_data['password'])
         user.save()
 
